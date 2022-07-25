50c78b60858f5ee58279324dbe14d47bda85432d
kamran miralami <kamran.m1380@yahoo.com>
2022-07-25 18:28:17 +0430

    running and camera

---
+++ Assets/Scripts_K/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    Vector3 offset;
+    private void Start()
+    {
+        offset = transform.position-target.position;
+    }
+    private void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
