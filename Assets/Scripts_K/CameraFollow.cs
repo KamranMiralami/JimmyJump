ba9cb7504d5feb6cb84b493e79edcde392fef7a7
kamran miralami <kamran.m1380@yahoo.com>
2022-07-27 12:48:44 +0430

    camera and enemy range indicator changes

--- Assets/Scripts_K/CameraFollow.cs
+++ Assets/Scripts_K/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    Vector3 offset;
+    public Vector3 offset;
     private void Start()
     {
         offset = transform.position-target.position;
