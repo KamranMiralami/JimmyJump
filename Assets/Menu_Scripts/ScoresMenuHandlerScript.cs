f2a85a3c3ffd259c28a931a4845f921732f557bb
Sepehr <62986692+IIIphr@users.noreply.github.com>
2022-07-27 12:41:19 +0430

    High score list is working now

--- Assets/Menu_Scripts/ScoresMenuHandlerScript.cs
+++ Assets/Menu_Scripts/ScoresMenuHandlerScript.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 [Serializable]
 public class HighScores
@@ -12,11 +14,29 @@ public class HighScores
 
 public class ScoresMenuHandlerScript : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI scoresText;
+
     // Start is called before the first frame update
     void Start()
     {
         string data = PlayerPrefs.GetString("scores", "");
         HighScores highScores = JsonUtility.FromJson<HighScores>(data);
+        setScoresList(highScores);
+    }
+
+    void setScoresList(HighScores highScores)
+    {
+        string text = "";
+        for(int i=0; i<highScores.names.Length; i++)
+        {
+            text += (highScores.names[i] + " --> " + highScores.scores[i] + "\n");
+        }
+        scoresText.text = text;
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     // Update is called once per frame
