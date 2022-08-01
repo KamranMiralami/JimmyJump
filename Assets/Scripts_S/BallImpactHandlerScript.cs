2ef0496ab69c8547fa6a8e7e392d0cb84740f012
Sepehr <62986692+IIIphr@users.noreply.github.com>
2022-08-01 16:16:06 +0430

    Death animation kinda works now

--- Assets/Scripts_S/BallImpactHandlerScript.cs
+++ Assets/Scripts_S/BallImpactHandlerScript.cs
@@ -17,6 +17,27 @@ public class BallImpactHandlerScript : MonoBehaviour
         if (other.gameObject.CompareTag("Ball"))
         {
             modelAnimation.SetBool("isDead", true);
+            StartCoroutine(resizeCollider());
+        }
+    }
+
+    public IEnumerator resizeCollider()
+    {
+        float initialHeight = transform.parent.GetComponent<CapsuleCollider>().height;
+        float initialRadius = transform.parent.GetComponent<CapsuleCollider>().radius;
+        float t = 0f;
+        while (t < 1.6)
+        {
+            t += Time.deltaTime;
+            yield return null;
+        }
+        t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            transform.parent.GetComponent<CapsuleCollider>().height = Mathf.Lerp(initialHeight, 0.2f, t);
+            transform.parent.GetComponent<CapsuleCollider>().radius = Mathf.Lerp(initialRadius, 0.2f, t);
+            yield return null;
         }
     }
 
