1adf4f92bfe503cd9282cb68ad620d892372b84f
Sepehr <62986692+IIIphr@users.noreply.github.com>
2022-08-10 14:30:13 +0430

    Changed the main menu style, needs more work

--- Assets/Scripts_S/MainMenuHandlerScript.cs
+++ Assets/Scripts_S/MainMenuHandlerScript.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuHandlerScript : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI levelNumber;
+    [SerializeField] int maxLevel;
+    int level = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,11 +17,33 @@ public class MainMenuHandlerScript : MonoBehaviour
         highScores.names = new string[] { "me", "you", "them" };
         highScores.scores = new int[] { 1, 2, 3 };
         PlayerPrefs.SetString("scores", JsonUtility.ToJson(highScores));
+        levelNumber.text = "1";
     }
 
     public void Levels()
     {
         SceneManager.LoadScene("Levels");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level" + (level + 1));
+    }
+
+    public void Next()
+    {
+        level = (level + 1) % maxLevel;
+        levelNumber.text = "" + (level + 1);
+    }
+
+    public void Previous()
+    {
+        level = (level - 1) % maxLevel;
+        if (level < 0)
+        {
+            level += maxLevel;
+        }
+        levelNumber.text = "" + (level + 1);
     }
 
     public void LoadHighScores()
