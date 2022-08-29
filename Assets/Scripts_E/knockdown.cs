1a73c8d65e7ecd8f5a958aaf4e069cd60a3e63ce
3RFUNn <efnoskuee@gmail.com>
2022-08-29 19:02:34 +0430

    npc knockdown

---
+++ Assets/Scripts_E/knockdown.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Scripts_E
+{
+    public class Knockdown : MonoBehaviour
+    {
+        [SerializeField] private Animator anim;
+        private static readonly int Fall = Animator.StringToHash("Fall");
+        private static readonly int Fall2 = Animator.StringToHash("Fall2");
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Front"))
+            {
+                anim.SetBool("Fall",true);
+            }
+            else if (other.CompareTag("Back"))
+            {
+                anim.SetBool("Fall2",true);
+            }
+        }
+    }
+}
