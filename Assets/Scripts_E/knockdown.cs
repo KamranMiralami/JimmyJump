7ca796da9ed1cc91cf5e5de631d864f7633c594a
3RFUNn <efnoskuee@gmail.com>
2022-08-31 15:23:46 +0430

    npc

--- Assets/Scripts_E/knockdown.cs
+++ Assets/Scripts_E/knockdown.cs
@@ -4,14 +4,21 @@ namespace Scripts_E
 {
     public class knockdown : MonoBehaviour
     {
-        [SerializeField] private Animator anim;
+        [SerializeField] private Animator anim, playeranim;
         private static readonly int Fall = Animator.StringToHash("Fall");
         private static readonly int Fall2 = Animator.StringToHash("Fall2");
+        private int rand;
+
+        private void Start()
+        {
+            
+        }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Front"))
             {
+                rand = Random.Range(0, 2);
                 anim.SetBool("Fall",true);
             }
             else if (other.CompareTag("Back"))
--- Assets/Scripts_E/soccerwalk.cs
+++ Assets/Scripts_E/soccerwalk.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,9 +11,23 @@ public class soccerwalk : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private Animator SoccerBehaviour;
     private bool stop = false;
+    private float turnSpeed = 0.75f;
+    private Vector3 dir, rotation;
+    private Quaternion lookRotation, partToRotate;
+
+     void Start()
+    {
+       
+        
+    }
 
     void Update()
     {
+        dir = target.transform.position - transform.position;
+        lookRotation = Quaternion.LookRotation(dir);
+        rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        
         if (!stop)
         {
             transform.position =
@@ -29,3 +44,4 @@ public class soccerwalk : MonoBehaviour
         }
     }
 }
+
